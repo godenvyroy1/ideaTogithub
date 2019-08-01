d750dd2f74e23f94e6fb5f3a6c3aa7da8b53074c
roy <1665616225@qq.com>
2019-08-01 14:58:12 +0800

    通过idea,添加测试信息到本地git仓库

---
+++ idea-git-test/src/cn/jiyun/test/GitTest.java
@@ -0,0 +1,11 @@
+package cn.jiyun.test;
+
+/**
+ * @author roy
+ * @create 2019-08-01 14:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("就是测试一个idea-git-test");
+    }
+}
